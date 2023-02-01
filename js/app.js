5d682cd3ede2e6a5e2c5ada4cf5e049ab31ab250
GraceGenuardi <graziagenuardi96@gmail.com>
2023-02-01 15:46:18 +0100

    EDIT definita struttura logica JS

---
+++ js/app.js
@@ -0,0 +1,16 @@
+///MILESTONE 1:
+
+
+//1- Inserisci Input con KM da percorrere 
+
+//2-selezionare da una select il tipo di sconto da applicare (3 scelte: sconto minorenne, sconto over 65, nessuno sconto)
+
+
+    //-SE <= 18 AVRAI UNO SCONTO DEL 20%
+        //-ALTRIMENTI Prezzo 0.21 € al km
+
+    //-SE >= 65 AVRAI UNO SCONTO DEL 40%
+        //-ALTRIMENTI Prezzo 0.21 € al km
+
+
+//3-Al click sul pulsante calcolare il prezzo del biglietto.
